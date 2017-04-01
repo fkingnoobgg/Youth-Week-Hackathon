a46fe1693741624b2e0a8bdf00642dc81cc80d48
Zen Ly <zen-ly@users.noreply.github.com>
2017-04-01 18:42:31 +0800

    Initial Models

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class HTUser(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    activation_key = models.CharField(max_length=64)
+    def __str__(self):
+        return str(self.user)
+
+class Node(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    longitude = models.FloatField(
+        validators=[MaxValueValidator(90), MinValueValidator(-90)]
+    )
+    latitude = models.FloatField(
+        validators = [MaxValueValidator(180), MinValueValidator(-180)]
+    )
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    votes_up = models.IntegerField(
+        default = 0,
+        validators = [MinValueValidator(0)]
+    )
+    votes_down = models.IntegerField(
+        default = 0,
+        validators = [MinValueValidator(0)]
+    )
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_modified = models.DateTimeField(auto_now=True)
+
+class Hotspot(models.Model):
+    node = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_modified = models.DateTimeField(auto_now=True)
+
+
+class Service(models.Model):
+    node = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_modified = models.DateTimeField(auto_now=True)
