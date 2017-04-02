113e2a02add8b930c4f70ac92c5c369990e593f7
Samuel Heath <sam.j.s.heath@gmail.com>
2017-04-02 10:35:50 +0800

    Admin Fields

--- hackathon/admin.py
+++ hackathon/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','description']
+
+    @admin.register(HTUser)
+    class HTUserAdmin(admin.ModelAdmin):
+        list_display = ['user','activation_key']
+
+@admin.register(Node)
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ['user','name','description','votes_up','votes_down','longitude','latitude','time_created','time_modified']
+    list_filter = ['time_created','time_modified']
